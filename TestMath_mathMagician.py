878bb8008200fb31ef1e24b4dc46ed1873a38b96
Castle Crawford <nathanscottcrawford@gmail.com>
2016-07-26 10:18:14 -0500

    set up basic tests for input and output

--- TestMath_mathMagician.py
+++ TestMath_mathMagician.py
@@ -7,16 +7,46 @@ class TestMagician(unittest.TestCase):
 def setUp(self):
 
   self.magic = Magician()
-
-def
-
-
-
+  self.input_is_int = 1
+  self.output = user_output
+  self.user_choice = input('I am the Math Magician. What would you like me to do? ')
 
 
+def Test_input_integers(self, number_to_output):
+
+  self.assertEqual(self.magic.print_integers(self.input_is_int), 1)
+  self.assertEqual(self.magic.print_integers(self.input_is_int), int)
+
+def Test_input_fibonacci(self, number_to_output):
+
+  self.assertEqual(self.magic.print_integers(self.input_is_int), 1)
+  self.assertEqual(self.magic.print_integers(self.input_is_int), int)
+
+
+def Test_input_primes(self, number_to_output):
+
+  self.assertEqual(self.magic.print_integers(self.input_is_int), 1)
+  self.assertEqual(self.magic.print_integers(self.input_is_int), int)
+
+def Test_print_integers(self, number_to_output):
+
+  self.assertEqual(self.magic.print_integers(self.input_is_int), 1)
+  self.assertEqual(self.magic.print_integers(self.input_is_int), int)
 
 
 
+
+def Test_print_fibonacci(self, number_to_output):
+
+  self.assertEqual(self.magic.print_fibonacci(self.input_is_int), 1)
+  self.assertEqual(self.magic.print_fibonacci(self.input_is_int), int)
+
+
+
+def Test_print_primes(self, number_to_output):
+
+  self.assertEqual(self.magic.print_primes(self.input_is_int), 1)
+  self.assertEqual(self.magic.print_primes(self.input_is_int), int)
 
 
 
