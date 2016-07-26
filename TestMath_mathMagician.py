7567b73046002143f58124b24fb2d801334f5080
Castle Crawford <nathanscottcrawford@gmail.com>
2016-07-26 09:21:18 -0500

    initial setup

---
+++ TestMath_mathMagician.py
@@ -0,0 +1,34 @@
+import unittest
+from mathMagician import *
+class TestMagician(unittest.TestCase):
+
+
+@classmethod
+def setUp(self):
+
+  self.magic = Magician()
+
+def
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  unittest.main()
